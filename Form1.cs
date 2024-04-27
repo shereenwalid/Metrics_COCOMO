af1fd8b74eaa23357129f03d7b075a6fec1022bf
Shery <Shery@DESKTOP-H7TCCM3>
2024-04-27 09:13:52 -0700

    Forms created

--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,14 @@ namespace Metrics_COCOMO
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Basic basic_form = new Basic();
+            basic_form.Show();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            Intermediate inter_form = new Intermediate();
+            inter_form.Show();
         }
 
         private void Form1_Load(object sender, EventArgs e)
